98d1663be6c21b69624f9a814b222b6bc24ccc3e
Maghe Audrey <82166472+AudreyLysette@users.noreply.github.com>
2021-04-21 22:30:03 +0200

    Create Conge.h

---
+++ Conge.h
@@ -0,0 +1,31 @@
+#ifndef CONGE_H
+#define CONGE_H
+#include <QString>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
+
+class conge
+
+{
+ public:
+    conge();
+    conge(int,QString,QString,QString,QString);
+    QString get_motif();
+    QString get_date_debut();
+    QString get_date_fin();
+    QString get_type();
+    int get_id();
+
+     bool ajouter();
+     bool modifier(int);
+     QSqlQueryModel * afficher();
+     bool supprimer(int);
+     QSqlQueryModel * trier();
+     QSqlQueryModel * chercher(QString);
+private:
+    QString motif,date_debut,date_fin,type;
+    int id;
+};
+
+
+#endif // CONGE_H
